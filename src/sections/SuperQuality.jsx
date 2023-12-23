dbe1e87970af3892f5bfe0eff150e1221334d2ce
jaseemuddin <jaseemuddinn@gmail.com>
2023-12-23 22:02:43 +0530

    product updated

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -13,7 +13,7 @@ function SuperQuality() {
                 </span> Shoes
             </h2>
             <p className="mt-4 lg:max-w-lg info-text">
-            Ensuring premium comfort and style, our meticulouslycrafted footwear is designed to elevate your experience,providing you with unmatched quality, innovation, and a touch of elegance.
+            Ensuring premium comfort and style, our meticulously crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation, and a touch of elegance.
             </p>
             <p className="mt-6 lg:max-w-lg info-text">
             Our dedication to detail and excellence ensures yoursatisfaction
