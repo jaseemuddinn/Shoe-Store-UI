16a97adf5f17d99c4945d3b87bcc75ad8e442f29
jaseemuddin <jaseemuddinn@gmail.com>
2023-12-24 00:36:51 +0530

    section updated

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -16,7 +16,7 @@ function SuperQuality() {
             Ensuring premium comfort and style, our meticulously crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation, and a touch of elegance.
             </p>
             <p className="mt-6 lg:max-w-lg info-text">
-            Our dedication to detail and excellence ensures yoursatisfaction
+            Our dedication to detail and excellence ensures your satisfaction
             </p>
             <div className="mt-11">
             <Button label="View details" />
